c598404d9230ac64b34a08b3ed0a383e3fbc4d58
Eduardo  Tenório <embatbr@gmail.com>
2013-07-24 20:53:26 -0300

    Lesson03 on the way

---
+++ lesson03/code/variables.py
@@ -0,0 +1,14 @@
+porcos = 3
+galinhas = 5
+vacas = 2
+animais = porcos + galinhas + vacas
+
+vacaPremiada = "Mimosa"
+
+print("Minha fazenda possui", porcos, "porcos,", galinhas, "galinhas e", vacas, "vacas")
+print("São no total", porcos + galinhas + vacas, "bichos")
+print("Mas nenhum é tão querido quanto", vacaPremiada)
+
+vacaPremiada = 200
+
+print("Minha vaca premiada pesa", vacaPremiada, "quilos")
