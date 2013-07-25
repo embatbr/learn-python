5db6505ebca7fbf1905f4ea97253a7417cb02a0f
Eduardo  Tenório <embatbr@gmail.com>
2013-07-25 17:52:58 -0300

    Increasing Lesson 03

---
+++ lesson03/code/stringFormat.py
@@ -0,0 +1,13 @@
+nome = 'Eduardo Tenório'
+idade = 27
+altura = 1.83
+peso = 110 #sou denso
+olhos = 'verdes'
+pele = 'bronzeada de LED'
+cabelo = 'ruim'
+
+print('Vamos falar sobre %s' % nome)
+print('Ele possui %1.2f de altura' % altura)
+print('Pesa %d quilos (densidade alta)' % peso)
+print('Possui charmosos olhos %s e pele %s' % (olhos, pele))
+print('E tem cabelo %s' % cabelo)
---
+++ lesson03/code/stringLine.py
@@ -0,0 +1,7 @@
+stringLine = 'linha 1\nlinha 2'
+stringMultLine = '''linha 1
+linha 2'''
+
+print(stringLine)
+print()
+print(stringMultLine)
